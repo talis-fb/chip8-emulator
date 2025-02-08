01dd1d9ccd92e6feb83377985ae8a8ecd0f15401
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2025-02-08 15:25:26 -0300

    feat: adjust main.go to be called in Wasm

--- core/main.go
+++ core/main.go
@@ -7,83 +7,6 @@ import (
 	"syscall/js"
 )
 
-var romData [4096]byte
-
-func setRom(this js.Value, args []js.Value) interface{} {
-	println("[WASM] setRom called")
-
-	jsArray := args[0]
-	tempRomData := make([]byte, jsArray.Length())
-
-	js.CopyBytesToGo(tempRomData, jsArray)
-
-	for i := 0; i < len(tempRomData); i++ {
-		romData[i+0x200] = tempRomData[i]
-	}
-
-	println("ROM loaded:", len(romData), "bytes")
-
-	return nil
-}
-
-var drawCallback js.Value
-
-func onDraw(this js.Value, args []js.Value) interface{} {
-	println("[WASM] onDraw called")
-	if args[0].Type() == js.TypeFunction {
-		drawCallback = args[0]
-	}
-
-	// screenBuffer := []byte{0x00, 0x01, 0xFF, 0x02}
-
-	// jsArray := js.Global().Get("Uint8Array").New(len(screenBuffer))
-	// for i, pixel := range screenBuffer {
-	// 	jsArray.SetIndex(i, pixel)
-	// }
-
-	// if drawCallback.Type() == js.TypeFunction {
-	// 	drawCallback.Invoke(jsArray)
-	// }
-
-	return nil
-}
-
-var soundCallback js.Value
-
-func onSound(this js.Value, args []js.Value) interface{} {
-	println("[WASM] onSound called")
-	if args[0].Type() == js.TypeFunction {
-		soundCallback = args[0]
-	}
-
-	return nil
-}
-
-var keyboard [16]bool
-
-func setKey(this js.Value, args []js.Value) interface{} {
-	println("[WASM] setKey called")
-
-	key := args[0].Int()
-	isPressed := args[1].Bool()
-
-	println(key, isPressed)
-
-	keyboard[key] = isPressed
-
-	return nil
-}
-
-func reset(this js.Value, args []js.Value) interface{} {
-	println("[WASM] reset called")
-	return nil
-}
-
-func cycle(this js.Value, args []js.Value) interface{} {
-	println("[WASM] cycle called")
-	return nil
-}
-
 func main() {
 	c := make(chan struct{}, 0)
 
@@ -105,4 +28,74 @@ func main() {
 	js.Global().Set("cycle", js.FuncOf(cycle))
 
 	<-c // Block the Go runtime from exiting
+}
+
+var chip8 Chip8
+
+// it assumes the client is running it 60Hz
+func cycle(this js.Value, args []js.Value) interface{} {
+	opcode := uint16(chip8.Memory[chip8.PC])<<8 | uint16(chip8.Memory[chip8.PC+1])
+
+	chip8.ExecuteOpcode(opcode)
+
+	chip8.PC += 2
+
+	return nil
+}
+
+func setRom(this js.Value, args []js.Value) interface{} {
+	println("[WASM] setRom called")
+
+	jsArray := args[0]
+	romData := make([]byte, jsArray.Length())
+
+	js.CopyBytesToGo(romData, jsArray)
+
+	for i := 0; i < len(romData); i++ {
+		chip8.Memory[i+0x200] = romData[i]
+	}
+
+	println("ROM loaded:", len(romData), "bytes")
+
+	return nil
+}
+
+var drawCallback js.Value
+
+func onDraw(this js.Value, args []js.Value) interface{} {
+	println("[WASM] onDraw called")
+	if args[0].Type() == js.TypeFunction {
+		drawCallback = args[0]
+	}
+	return nil
+}
+
+var soundCallback js.Value
+
+func onSound(this js.Value, args []js.Value) interface{} {
+	println("[WASM] onSound called")
+	if args[0].Type() == js.TypeFunction {
+		soundCallback = args[0]
+	}
+
+	return nil
+}
+
+func setKey(this js.Value, args []js.Value) interface{} {
+	println("[WASM] setKey called")
+
+	key := args[0].Int()
+	isPressed := args[1].Bool()
+
+	println(key, isPressed)
+
+	chip8.Keyboard[key] = isPressed
+
+	return nil
+}
+
+func reset(this js.Value, args []js.Value) interface{} {
+	println("[WASM] reset called")
+	chip8 = Chip8{}
+	return nil
 }
